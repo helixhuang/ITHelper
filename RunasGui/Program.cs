17b0d24d81a7702f5c121445f7482bfc256c628f
Helix Huang <huangjianping@gmail.com>
2015-03-16 17:10:16 +0800

    添加发送到桌面功能

--- RunasGui/Program.cs
+++ RunasGui/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace RunasGui
@@ -10,8 +13,22 @@ namespace RunasGui
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args != null && args.Length > 0)
+            {
+                string appName = args[0];
+                List<RunasObject> runasList = JsonConvert.DeserializeObject<List<RunasObject>>(File.ReadAllText("RunasData.json", Encoding.UTF8));
+                if (runasList != null)
+                {
+                    RunasObject runasObj = runasList.Find(o => o.Name == appName);
+                    if (runasObj != null)
+                    {
+                        RunasHelper.Runas(runasObj);
+                        return;
+                    }
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
